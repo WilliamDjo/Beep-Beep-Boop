150f92e326904ebb671695c54b668b3d13e374b8
WilliamDjo <william.djong@yahoo.com>
2025-05-17 00:43:56 +1000

    Completed

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Simple python code that when you press a number key (e.g. "3") the terminal will beep that many times
+Simple python code that when you press a number key (e.g. "3") the terminal will beep that many times.
+Trying to play with low level terminal functions.
--- beep_beep_boop.py
+++ beep_beep_boop.py
@@ -1,3 +1,16 @@
 import sys
+import tty
+import termios
 
-sys.stdout.write.buffer(b"\x07")
+tty.setcbreak(0)
+old_settings = termios.tcgetattr(0)
+
+try:
+    while True:
+        ch = sys.stdin.read(1)
+        if "1" <= ch <= "9":
+            for _ in range(int(ch)):
+                sys.stdout.buffer.write(b"\x07")
+        sys.stdout.buffer.flush()
+finally:
+    termios.tcsetattr(0, termios.TCSADRAIN, old_settings)
